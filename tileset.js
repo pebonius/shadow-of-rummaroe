96f9836446b00692a6ba3bd2bcff9f1b21e15c02
pebonius <pebonius@gmail.com>
2024-07-20 06:44:16 +0200

    adding house

--- tileset.js
+++ tileset.js
@@ -4,7 +4,7 @@ export default class Tileset {
   constructor(gameScreen) {
     this.gameScreen = gameScreen;
     this.image = gameScreen.content.getAssetByName("tileset");
-    this.walkableTiles = [255, 191, 190, 189];
+    this.walkableTiles = [13, 15, 189, 190, 191, 197, 213, 255];
     this.spikeTiles = [219, 220, 235, 236];
     this.platformTiles = [108];
     this.tileSize = 16;
